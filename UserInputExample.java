df6d4ac7b4380ab6e6e889502b58a8bb3902b34d
sameer <bv014010@gmail.com>
2026-02-11 23:57:34 +0530

    initial

---
+++ UserInputExample.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class UserInputExample {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Taking String input
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        // Taking integer input
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        // Taking double input
+        System.out.print("Enter your height (in meters): ");
+        double height = sc.nextDouble();
+
+        System.out.println("\n--- User Details ---");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " m");
+
+        sc.close();
+    }
+}
