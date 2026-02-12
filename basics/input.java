df48b4ba3a178eae41f3bc47524dabb95b568412
sameer <bv014010@gmail.com>
2026-02-13 01:58:57 +0530

    hello

--- basics/input.java
+++ basics/input.java
@@ -6,7 +6,7 @@ public class input {
         Scanner sc = new Scanner(System.in);
 
         // Taking String input
-        System.out.print("Enter your name pls: ");
+        System.out.print("Enter your name  sir: ");
         String name = sc.nextLine();
 
         // Taking integer input
